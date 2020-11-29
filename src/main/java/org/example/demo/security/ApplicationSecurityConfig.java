e1b3c2c8681e18eaf4bca67c154a3bfee7a9a475
Can Zhao <canzhao216@gmail.com>
2020-11-29 13:42:16 -0500

    before add jwt

--- src/main/java/org/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/org/example/demo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,17 @@
 package org.example.demo.security;
 
+import org.example.demo.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,16 +23,112 @@ import java.util.concurrent.TimeUnit;
 import static com.google.common.base.Predicates.and;
 import static org.example.demo.security.ApplicationUserRole.*;
 
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http  //order matters
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+//                .antMatchers("/api/**").hasRole(STUDENT.name())
+////                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+////                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+////                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+////                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").permitAll()
+//                .defaultSuccessUrl("/courses", true)
+//                .and()
+//                .rememberMe()
+//                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))   // default 30 mins
+//                    .key("somethingverysecured")  //used to hash contents of username and expiration time
+//                .and()
+//                .logout()
+//                    .logoutUrl("/logout")
+//                    .clearAuthentication(true)
+//                    .invalidateHttpSession(true)
+//                    .deleteCookies("JSESSIONID", "remember-me")
+//                    .logoutSuccessUrl("/login");
+//    }
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService(){
+//        UserDetails annaSmithUser = User.builder()
+//                .username("annasmith")
+//                .password("password") // will throw exception because no password encoder here
+//                .roles("STUDENT") //ROLE_STUDENT
+//                .build();
+//        return new InMemoryUserDetailsManager(
+//                annaSmithUser
+//        );
+//    }
+//
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService(){
+//        UserDetails annaSmithUser = User.builder()
+//                .username("annasmith")
+//                .password(passwordEncoder.encode("password")) // passwordEncoder done!
+////                .roles(STUDENT.name()) //ROLE_STUDENT
+//                .authorities(STUDENT.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails lindaUser = User.builder()
+//            .username("linda")
+//            .password(passwordEncoder.encode("password123"))
+////            .roles(ADMIN.name()) // you can set multiple roles to a person
+//            .authorities(ADMIN.getGrantedAuthorities())
+//            .build();
+//
+//        UserDetails tomUser = User.builder()
+//                .username("tom")
+//                .password(passwordEncoder.encode("password123"))
+////                .roles(ADMINTRAINEE.name()) // you can set multiple roles to a person
+//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(
+//                annaSmithUser,
+//                lindaUser,
+//                tomUser
+//        );
+//    }
+//}
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
+//    private final JwtConfig jwtConfig;
+//    private final SecretKey secretKey;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+                                     ApplicationUserService applicationUserService
+//                                     JwtConfig jwtConfig,
+//                                     SecretKey secretKey
+    ) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
+//        this.jwtConfig = jwtConfig;
+//        this.secretKey = secretKey;
     }
 
     @Override
@@ -46,8 +146,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true)
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/courses", true)
                 .and()
                 .rememberMe()
                     .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))   // default 30 mins
@@ -61,47 +161,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutSuccessUrl("/login");
     }
 
-//    @Override
-//    @Bean
-//    protected UserDetailsService userDetailsService(){
-//        UserDetails annaSmithUser = User.builder()
-//                .username("annasmith")
-//                .password("password") // will throw exception because no password encoder here
-//                .roles("STUDENT") //ROLE_STUDENT
-//                .build();
-//        return new InMemoryUserDetailsManager(
-//                annaSmithUser
-//        );
-//    }
-
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    protected UserDetailsService userDetailsService(){
-        UserDetails annaSmithUser = User.builder()
-                .username("annasmith")
-                .password(passwordEncoder.encode("password")) // passwordEncoder done!
-//                .roles(STUDENT.name()) //ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
 
-        UserDetails lindaUser = User.builder()
-            .username("linda")
-            .password(passwordEncoder.encode("password123"))
-//            .roles(ADMIN.name()) // you can set multiple roles to a person
-            .authorities(ADMIN.getGrantedAuthorities())
-            .build();
-
-        UserDetails tomUser = User.builder()
-                .username("tom")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMINTRAINEE.name()) // you can set multiple roles to a person
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                annaSmithUser,
-                lindaUser,
-                tomUser
-        );
+        return provider;
     }
 }
